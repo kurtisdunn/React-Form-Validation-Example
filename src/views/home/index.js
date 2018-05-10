4da35e9836af81e1418cc669139153683297e177
Kurtis Dunn <kdunn@Kurtiss-Mac-Pro.local>
2018-05-10 18:40:15 +1000

    remove comments

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -9,11 +9,6 @@ import Input from '../../components/input';
 import Select from '../../components/select';
 import Textarea from '../../components/textarea';
 
-// onChange function etc
-function someFunction(){
-  return null;
-}
-
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -22,35 +17,15 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <h1 className="mb3">React Form Valdation Example</h1>
+        <h1>React Form Valdation Example</h1>
         <Form method={ ContactPost }>
-          {/* <Select title={'Title'} onChange={ someFunction } name={'title'} validator={'required'} options={[{name:'Mr', option:'mr'}, {name:'Mrs', option:'mrs'}] }/> */}
-          {/* <div className="row"> */}
-            {/* <div className="col-md-6 mb-3"> */}
-              <Input title={'First Name'} name={'firstName'} validator={'required'} />
-            {/* </div> */}
-            {/* <div className="col-md-6 mb-3"> */}
-              <Input title={'Last Name'} name={'lastName'} validator={'required'}/>
-            {/* </div> */}
-          {/* </div> */}
-          {/* <div className="mb-3"> */}
-            <Input title={'Email'} name={'email'} type={'email'} validator={'required, email'} />
-          {/* </div> */}
-          {/* <div className="mb-3"> */}
-            <Input title={'Address'} name={'address'} validator={'required'} />
-          {/* </div> */}
-          {/* <div className="mb-3"> */}
-            <Input title={'Address 2'} name={'address2'} />
-          {/* </div> */}
-          {/* <div className="mb-3"> */}
-            <Input title={'Email'} name={'email'} type={'email'} validator={'required, email'} />
-          {/* </div> */}
-          {/* <div className="mb-3"> */}
-            <Textarea rows={'10'} title={'Message'} name={'message'} validator={'required'} />
-          {/* </div> */}
-
-            <Button value={'Send!'} class={'btn-submit'} type={'submit'} />
-
+          <Input title={'First Name'} name={'firstName'} validator={'required'} />
+          <Input title={'Last Name'} name={'lastName'} validator={'required'}/>
+          <Input title={'Email'} name={'email'} type={'email'} validator={'required, email'} />
+          <Input title={'Address'} name={'address'} validator={'required'} />
+          <Input title={'Address 2'} name={'address2'} />
+          <Textarea rows={'10'} title={'Message'} name={'message'} validator={'required'} />
+          <Button value={'Send!'} class={'btn-submit'} type={'submit'} />
         </Form>
       </div>
     );
